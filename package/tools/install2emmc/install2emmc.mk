db311648f95ee5f2717e1ab6be3f7904251e6d20
Juliano Dorigão <jdorigao@gmail.com>
2024-09-09 12:15:21 -0400

    add: package install2emmc

---
+++ package/tools/install2emmc/install2emmc.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# install2emmc
+#
+#############################################################
+
+INSTALL2EMMC_VERSION = 0.1
+INSTALL2EMMC_SOURCE =
+INSTALL2EMMC_PATH = $(BR2_EXTERNAL_RESCUE_PATH)/package/tools/install2emmc/src
+
+define INSTALL2EMMC_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/bin/
+	install -m 0755 $(INSTALL2EMMC_PATH)/install2emmc \
+		$(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(generic-package))
